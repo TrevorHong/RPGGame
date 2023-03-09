08cd8c9ed6ab37687981f481de5e28645f8e7c09
TrevorHong <65141699+TrevorHong@users.noreply.github.com>
2023-03-09 00:54:49 -0800

    Fixed movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel.Design.Serialization;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+
+{
+    private float Move;
+    public float speed;
+    public float jump;
+    private bool isFacingRight = true;
+
+    private Rigidbody2D rb;
+
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+    }   
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move = Input.GetAxis("Horizontal");
+        FlipSprite();
+        rb.velocity = new Vector2(Move * speed, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump") && isGrounded())
+        {
+            rb.AddForce(new Vector2(rb.velocity.x, jump));
+        }
+
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+
+
+    }
+
+    private bool isGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private void FlipSprite()
+    {
+        if(isFacingRight && Move < 0f || !isFacingRight && Move > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+}
