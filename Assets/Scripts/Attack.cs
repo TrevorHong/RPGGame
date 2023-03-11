9e7fa6d95760a4207ec3d0a49f3095f16cdf6c3d
TrevorHong <65141699+TrevorHong@users.noreply.github.com>
2023-03-10 16:34:28 -0800

    Attacking animation finished

--- Assets/Scripts/Attack.cs
+++ Assets/Scripts/Attack.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
+
+    public Animator animate;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,9 @@ public class Attack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetButtonDown("Fire1"))
+        {
+            animate.SetTrigger("Attacking");
+        }
     }
 }
