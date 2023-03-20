9099b3c2adfd6a59dc25c416c09f4d41b7e16825
TrevorHong <65141699+TrevorHong@users.noreply.github.com>
2023-03-20 16:10:52 -0700

    Attack hit detection now works

--- Assets/Scripts/Attack.cs
+++ Assets/Scripts/Attack.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using System.Threading;
 using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
-
+    //Calling animator here to 
     public Animator animate;
+
+    public Transform attackpos;
+    public float range = 0.5f;
+    public LayerMask enemies;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +25,26 @@ public class Attack : MonoBehaviour
         if(Input.GetButtonDown("Fire1"))
         {
             animate.SetTrigger("Attacking");
+            MeleeAttack();
         }
+    }
+    void MeleeAttack()
+    {
+        Collider2D[] attackhit = Physics2D.OverlapCircleAll(attackpos.position, range, enemies);
+
+        foreach (Collider2D enemy in attackhit)
+        {
+            Debug.Log("works");
+        }
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackpos == null)
+        {
+            return;
+        }
+        Gizmos.DrawWireSphere(attackpos.position, range);
     }
 }
