fad77788c0dfb2a25af3fbdef1af02b7732a72c5
TrevorHong <65141699+TrevorHong@users.noreply.github.com>
2023-04-05 19:09:56 -0700

    Fireball stuff

--- Assets/EnemyProjectile.cs
+++ Assets/EnemyProjectile.cs
@@ -7,24 +7,19 @@ public class EnemyProjectile : MonoBehaviour
 
     [SerializeField] Transform player;
 
-    public float detectrange;
-    public GameObject projectile;
-    public Transform offset;
+    public Rigidbody2D rb;
 
-    public int cooldown;
     public int projectilespeed;
 
+    
+    void Start()
+    {
+        rb.velocity = transform.right * projectilespeed;
+    }
     // Update is called once per frame
     void Update()
     {
-        transform.position = transform.right * Time.deltaTime * projectilespeed;
 
-        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
-
-        if (distanceToPlayer < detectrange)
-        {
-            Instantiate(projectile, offset.position, transform.rotation);
-        }
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
